b44156ff8b83a0eacfc43d9ad76657423775bfdb
maidazuha <projectacc.zm@gmail.com>
2018-09-01 14:20:57 +0100

    new tests added and removed

--- index.js
+++ index.js
@@ -3,13 +3,11 @@ function jobSorter(jobs) {
   if (jobs.length === 0) return [];
 // if (typeof jobs !== String) return "input must be in a string format!";
 
-//the input must be a string, therefore we can convert strings into an object using the json parse method.
-//eg '{z:z}' {z:z} and array as [z:z].
+// we can convert strings into an object E.G '{z:z}' {z:z} and array as [z:z].
   let orderedJobs = [];
   let jobsObj = JSON.parse(jobs);
-  //once converted, we can loop through the objects keys and can check that if the key for example in {z:z}
-  //if z is the same as its key value pair, which in this case is also a z, we can throw an error message as jobs
-  //cant depend on themeselves.
+  // we can loop through the objects keys and can check that if the key is the same as its key value pair,, 
+  //we can throw an error message as jobs cant depend on themeselves.
   for (let currentJob in jobsObj) {
     //return an error if a job depends upon itself
     if (jobsObj[currentJob] === currentJob) return "Sorry, but a job cant depend on itself!!";
@@ -18,13 +16,17 @@ function jobSorter(jobs) {
     if (jobsObj[currentJob].length === orderedJobs.length) {
       if (!orderedJobs.includes(currentJob)) orderedJobs.push(currentJob);
     }
-    // if it does include currentJob and job dependency in the array return an error. If there is no currentJob or a dependency value then put the jobs in order and push into the array.
+    // if it does include currentJob and job dependency in the array return an error. 
+    //If there is no currentJob or a dependency value then put the jobs in order and push into the array.
     else {
+
       if (orderedJobs.includes(jobsObj[currentJob]) && orderedJobs.includes(currentJob)) return "Sorry, jobs cant have circular dependencies!!";
       else if (!orderedJobs.includes(jobsObj[currentJob]) && !orderedJobs.includes(currentJob)) {
         orderedJobs.push(jobsObj[currentJob], currentJob);
            }
+    
     }
+    
   }
   return orderedJobs;
 
--- spec/index.spec.js
+++ spec/index.spec.js
@@ -3,19 +3,13 @@ const  jobSorter  = require("../index.js");
 
 describe("jobsSorter", () => {
   it("if there are no jobs, returns an empty array.", () => {
-    const input = "";
-    const actual = jobSorter(input);
+    let input = "";
+    let actual = jobSorter(input);
     expect(actual).to.eql([]);
   });
-  it("expect jobSorter to be a function", () => {
-    expect(jobSorter).to.be.a('function')
-  });
-  it('expect jobSorter to have one parameter', () => {
-    expect(jobSorter.length).to.equal(1);
-  });
   it("if only one job passed without dependencies, returns that element in an array", () => {
-    input = '{"z": ""}';
-    actual = jobSorter(input);
+    let input = '{"z": ""}';
+    let actual = jobSorter(input);
     expect(actual).to.eql(["z"]);
   });
   it("if only one job passed without dependencies, returns that element in an array", () => {
@@ -23,13 +17,29 @@ describe("jobsSorter", () => {
     let actual = jobSorter(input);
     expect(actual).to.eql(["a"]);
   });
+  it("returns an array of elements in no particular order with no dependencies", () => {
+    let input = '{"a": "", "b": "", "c": ""}';
+    let actual = jobSorter(input);
+    expect(actual).to.be.an("array").that.includes("b", "a", "c");
+  });
+  it("if no jobs depend on themselves, return them in an array", () => {
+    let input = '{"a": "", "b": "" }';
+    let actual = jobSorter(input);
+    expect(actual).to.be.an("array").that.includes("a", "b");
+  });
+
+  it("returns an array in the correct order including jobs with dependencies", () => {
+    let input = '{"a": "", "b": "", "c": ""}';
+    let actual = jobSorter(input);
+    expect(actual).to.be.an("array").that.includes("a", "b", "c");
+  });
   it("if one job depends on itself, returns an error message.", () => {
     let input = '{"p": "p"}';
     let actual = jobSorter(input);
     expect(actual).to.equal("Sorry, but a job cant depend on itself!!");
   });
   it("if two jobs depends on themselves, returns an error message.", () => {
-    let input = '{"z": "z","s":"s"}';
+    let input = '{"s":"s","t": ""}';
     let actual = jobSorter(input);
     expect(actual).to.equal("Sorry, but a job cant depend on itself!!");
   });
@@ -38,6 +48,7 @@ describe("jobsSorter", () => {
     let actual = jobSorter(input);
     expect(actual).to.equal("Sorry, but a job cant depend on itself!!");
   });
+
   it("returns an error when passed jobs that include circular dependencies", () => {
     let input = '{"a": "b", "b": "a", "c": "c"}';
     let actual = jobSorter(input);
@@ -54,13 +65,13 @@ describe("jobsSorter", () => {
     expect(actual).to.eql(["b", "c"]);
   });
   it("if two jobs have dependencies,return the elements in the correct order ",()=>{
-    input = '{"z": "x", "b":"a"}';
-    actual = jobSorter(input);
+    let input = '{"z": "x", "b":"a"}';
+    let actual = jobSorter(input);
     expect(actual).to.eql(["x","z","a","b"]);
 })
   it("if multiple jobs have dependencies,return the elements in the correct order ", () => {
-    input = '{"a": "b", "c":"d", "e":"f", "g":"h"}';
-    actual = jobSorter(input);
+    let input = '{"a": "b", "c":"d", "e":"f", "g":"h"}';
+    let actual = jobSorter(input);
     expect(actual).to.eql(["b", "a", "d", "c","f", "e", "h", "g"]);
   })
   it("if jobs depend on themselves along with jobs with no dependencies,return error message", () => {
@@ -68,4 +79,6 @@ describe("jobsSorter", () => {
     let actual = jobSorter(input);
     expect(actual).to.equal("Sorry, but a job cant depend on itself!!");
   });
+
 })
+
